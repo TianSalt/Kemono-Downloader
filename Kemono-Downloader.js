ceb1b62286c8fe2ecc2758c7da66c2e1de08d479
Akira Katsu <tiansalt@outlook.com>
2025-03-27 17:12:13 +0800

    Version 1.0

---
+++ Kemono-Downloader.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         Kemono Picture Downloader
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-17
+// @description  Add a button to download all pictures in a post.
+// @author       TianSalt
+// @match        https://*.kemono.su/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=kemono.su
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const url = new URL(window.location.href);
+    const isDownloadPage = new RegExp("https://n\\d\\.kemono\\.su/\\?a=").test(url.href);
+    const isImagePage = new RegExp("https://n\\d\\.kemono\\.su/data/").test(url.href);
+    if (!isDownloadPage && !isImagePage) {
+        let button = document.createElement("button");
+        button.innerText = "⭳";
+        button.style.position = "fixed";
+        button.style.bottom = "10px";
+        button.style.right = "10px";
+        button.style.zIndex = "1000";
+        button.style.fontSize = "30px";
+        button.style.padding = "0px 10px";
+        button.style.color = "#e9a17d";
+        button.style.backgroundColor = "#1d1f20";
+        button.style.border = "2px solid #e9a17d";
+        button.style.borderRadius = "4px";
+        button.style.transition = "background-color 0.1s, transform 0.1s";
+        button.onmouseover = function() { button.style.backgroundColor = "#5c330a"; };
+        button.onmouseout = function() { button.style.backgroundColor = "#1d1f20"; };
+
+        button.onclick = function() {
+            const imageUrlObjs = Array.from(document.querySelectorAll("a[class='fileThumb image-link']")).map(a => new URL(a.href));
+            let arr = [[], [], [], []];
+            const len = imageUrlObjs.length;
+            if (len === 0) {
+                alert("There's no artwork on this page.");
+                return;
+            }
+            for (let i = 0; i < len; i++) {
+                const hostname = imageUrlObjs[i].hostname;
+                const href = imageUrlObjs[i].href;
+                arr[hostname[1] - 1].push(href);
+            }
+            for (let i = 0; i < 4; i++) {
+                if (arr[i].length) {
+                    const links = encodeURIComponent(JSON.stringify(arr[i]));
+                    setTimeout(() => { window.open(`https://n${i+1}.kemono.su/?a=${links}`, `_wnd${i}`); }, 0);
+                }
+            }
+        };
+        document.body.appendChild(button);
+
+    } else {
+        const params = new URLSearchParams(url.search);
+        const links = JSON.parse(decodeURIComponent(params.get("a")));
+        for (let link of links) {
+            let a = document.createElement("a");
+            a.href = encodeURI(link);
+            a.download = "";
+            a.click();
+            console.log(a);
+        }
+        window.close();
+    }
+
+})();
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Kemono-Downloader
+
+Download all pictures in a [Kemono](https://kemono.su/) artwork page in one button.
+
+Install via [Tampermonkey](https://github.com/Tampermonkey/tampermonkey), and **allow pop-ups of Kemono** ([Chome](https://support.google.com/chrome/answer/95472?hl=en&co=GENIE.Platform%3DDesktop) | [Firefox](https://support.mozilla.org/en-US/kb/pop-blocker-settings-exceptions-troubleshooting) | [Edge](https://support.microsoft.com/en-us/microsoft-edge/block-pop-ups-in-microsoft-edge-1d8ba4f8-f385-9a0b-e944-aa47339b6bb5)) **before usage**.
+
+## Procedure
+
+Select all pictures by the pattern:
+
+```JavaScript
+document.querySelectorAll("a[class='fileThumb image-link']")
+```
+
+Pictures of kemono are saved in `https://n[1|2|3|4].kemono.su/?a=[link]`, but browsers nowadays don't allow cross-origin picture downloading (the pictures will just be opened in new tabs), so we need to download in pop-ups using `window.open` (that's why you need to allow pop-ups).
